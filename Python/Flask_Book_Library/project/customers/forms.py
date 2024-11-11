7976ba1a1325cb48a39168eb48b48a3cff694f34
Michał Dondajewski <michal.dondajewski@cert.pl>
2024-11-11 19:53:02 +0100

    Add Regex input validators to StringFields to avoid XSS

--- Python/Flask_Book_Library/project/customers/forms.py
+++ Python/Flask_Book_Library/project/customers/forms.py
@@ -1,12 +1,12 @@
 # Form imports
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp
 
 
 # Flask forms (wtforms) allow you to easily create forms in this format:
 class CreateCustomer(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])  
-    city = StringField('City', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Regexp(r'^[a-zA-Z0-9\s]+$')])
+    city = StringField('City', validators=[DataRequired(), Regexp(r'^[a-zA-Z0-9\s]+$')])
     age = IntegerField('Age', validators=[DataRequired()])
     submit = SubmitField('Create Customer')
